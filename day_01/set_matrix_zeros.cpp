d8fd731a0d0c5b7e2cee4383799190a3e5b46d1b
JatinKumarShandilya <sigmajks2001@gmail.com>
2023-06-06 22:08:09 +0530

    set_matrix_0

---
+++ day_01/set_matrix_zeros.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+// optimal solution => O(n*m) => O(n^2 approx) and SC => O(1)
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	int row = matrix.size();
+	int col = matrix[0].size();
+	int col0 = 1;
+	// extraRow = matrix[..][0]
+	// extraCol = matrix[0][..]
+
+	for(int i=0; i<row; i++){
+		for(int j=0; j<col; j++){
+			if(matrix[i][j] == 0){
+				// mark the ith row 
+				matrix[i][0] = 0;
+				// mark the jth col
+				if(j!=0)	
+					matrix[0][j] = 0;
+				else 		
+					col0 = 0;
+			}
+		}
+	}
+	for(int i=1; i<row; i++){
+		for(int j=1; j<col; j++){
+			if(matrix[i][j] != 0){
+				//check for col and row
+				if(matrix[i][0] == 0 || matrix[0][j] == 0)
+					matrix[i][j] = 0;
+			}
+		}
+	}
+	if(matrix[0][0] == 0){
+		for(int j=0; j<col; j++)	matrix[0][j] = 0;
+	}
+	if(col0 == 0){
+		for(int i=0; i<row; i++)	matrix[i][0] = 0;
+	}
+	
+}
