06864ac34e62c20078258a7f716e661d2dd70602
JatinKumarShandilya <sigmajks2001@gmail.com>
2023-06-20 10:47:01 +0530

    day_06

---
+++ day_06/detect_a_cycle_in_linkedlist.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+
+bool detectCycle(Node *head)
+{
+	if(head == NULL || head->next == NULL){
+        return false;
+    }
+    Node *slow = head;
+    Node *fast = head;
+
+    while(fast->next && fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(fast == slow){
+            return true;
+        }
+    }
+    return false;
+}
